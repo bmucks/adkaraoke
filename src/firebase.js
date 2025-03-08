33cc5e1e26fca978286d388267322c197c15fbfe
unknown <brandonmueckl@yahoo.com>
2025-03-08 17:04:46 -0500

    Update App.js with new functionality

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+
+  
+  import "firebase/firestore";
+  import firebase from 'firebase/compat/app';
+  import 'firebase/compat/auth';
+  import 'firebase/compat/firestore';
+  var firebaseConfig = {
+    apiKey: "AIzaSyAL3du6nMCQ0flZ1XzzVJIY0HR2cZbCNwQ",
+    authDomain: "goodbarkaraoke.firebaseapp.com",
+    projectId: "goodbarkaraoke",
+    storageBucket: "goodbarkaraoke.appspot.com",
+    messagingSenderId: "443669429939",
+    appId: "1:443669429939:web:28f5a2ae8bf660acdabac6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase;
